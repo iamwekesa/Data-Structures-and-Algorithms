4fc1cf4aeb6e24adf2869ec8886dfa1f62ea23f3
Dennis E. Wekesa <iamwexx@gmail.com>
2021-11-29 13:20:13 +0300

    Fix Dynamic Array Class and add additional test

--- app/src/main/java/DataStructuresAndAlgorithms/DataStructures/Arrays/DynamicArray.java
+++ app/src/main/java/DataStructuresAndAlgorithms/DataStructures/Arrays/DynamicArray.java
@@ -1,39 +1,16 @@
 
 package DataStructuresAndAlgorithms.DataStructures.Arrays;
 
-interface IArray_<T> {
-
-    public int getSize();
-
-    public T get(int index);
-
-    public void set(int index, T val);
-
-    public void add(T elem);
-
-    public void removeAt(int rm_index);
-
-    public boolean remove(T elem);
-
-    public boolean isEmpty();
-
-}
-
 /**
  * DynamicArrays : Integer DynamicArray
  */
 @SuppressWarnings("unchecked")
-public class DynamicArray<T> implements IArray_<T> {
-
-    // Actual DynamicArray size
-    private int capacity = 0;
-
-    // length user thinks DynamicArray is
-    private int len = 0;
+public class DynamicArray<T> implements Iterable<T> {
 
     private T[] arr;
+    private int len = 0; // length user thinks array is
+    private int capacity = 0; // Actual array size
 
-    // Initialize default capacity of 16 in the case where capacity is not defined
     public DynamicArray() {
         this(16);
     }
@@ -45,12 +22,12 @@ public class DynamicArray<T> implements IArray_<T> {
         arr = (T[]) new Object[capacity];
     }
 
-    public int getSize() {
+    public int size() {
         return len;
     }
 
     public boolean isEmpty() {
-        return len == 0;
+        return size() == 0;
     }
 
     public T get(int index) {
@@ -61,24 +38,36 @@ public class DynamicArray<T> implements IArray_<T> {
         arr[index] = elem;
     }
 
+    public void clear() {
+        for (int i = 0; i < len; i++)
+            arr[i] = null;
+        len = 0;
+    }
+
     public void add(T elem) {
+
+        // Time to resize!
         if (len + 1 >= capacity) {
             if (capacity == 0)
                 capacity = 1;
             else
                 capacity *= 2; // double the size
             T[] new_arr = (T[]) new Object[capacity];
-            for (int i = 0; i < new_arr.length; i++)
+            for (int i = 0; i < len; i++)
                 new_arr[i] = arr[i];
-            arr = new_arr; // new_arr has extra nulls padded
+            arr = new_arr; // arr has extra nulls padded
         }
+
         arr[len++] = elem;
+
     }
 
-    // Removes the element at the specified index in this list.
-    public void removeAt(int rm_index) {
-        if (rm_index < 0 || rm_index >= len)
+    // Removes an element at the specified index in this array.
+    public T removeAt(int rm_index) {
+        if (rm_index >= len && rm_index < 0) {
             throw new IndexOutOfBoundsException();
+        }
+        T data = arr[rm_index];
         T[] new_arr = (T[]) new Object[len - 1];
         for (int i = 0, j = 0; i < len; i++, j++)
             if (i == rm_index)
@@ -86,18 +75,56 @@ public class DynamicArray<T> implements IArray_<T> {
             else
                 new_arr[j] = arr[i];
         arr = new_arr;
-        --len;
-        --capacity;
+        capacity = --len;
+        return data;
     }
 
-    public boolean remove(T elem) {
+    public boolean remove(Object obj) {
+        int index = indexOf(obj);
+        if (index == -1)
+            return false;
+        removeAt(index);
+        return true;
+    }
+
+    public int indexOf(Object obj) {
         for (int i = 0; i < len; i++) {
-            if (arr[i].equals(elem)) {
-                removeAt(i);
-                return true;
+            if (obj == null) {
+                if (arr[i] == null)
+                    return i;
+            } else {
+                if (obj.equals(arr[i]))
+                    return i;
             }
         }
-        return false;
+        return -1;
+    }
+
+    public boolean contains(Object obj) {
+        return indexOf(obj) != -1;
+    }
+
+    // Iterator is still fast but not as fast as iterative for loop
+    @Override
+    public java.util.Iterator<T> iterator() {
+        return new java.util.Iterator<T>() {
+            int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < len;
+            }
+
+            @Override
+            public T next() {
+                return arr[index++];
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
     }
 
     @Override
@@ -110,5 +137,6 @@ public class DynamicArray<T> implements IArray_<T> {
                 sb.append(arr[i] + ", ");
             return sb.append(arr[len - 1] + "]").toString();
         }
+
     }
 }
